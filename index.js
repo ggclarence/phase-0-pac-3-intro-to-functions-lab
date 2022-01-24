ce55d2e9ecfb10d5c57279a7f3f412846ecad071
Eric Van <ggclarence@yahoo.com>
2022-01-24 02:25:29 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase(); 
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+   var upper = "YES INDEED!"
+   var lower = "I can't hear you!"
+   var normal = `I would love to!`
+   if (string.toLowerCase(string) === string) {
+       return lower;
+   }
+   else if (string.toUpperCase(string) === string) {
+       return upper;
+   }
+   else if (`Let's have dinner together!` === string) {
+       return normal;
+   }
+}
